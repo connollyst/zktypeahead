cd827a261b2c28a90127178fafde8ab0e6432b6c
Sean Connolly <connolly.st@gmail.com>
2016-03-23 14:46:06 +1000

    Bloodhound example with hardcoded 'states' data

--- src/main/resources/web/js/zul/typeahead/Typeahead.js
+++ src/main/resources/web/js/zul/typeahead/Typeahead.js
@@ -2,27 +2,6 @@ zul.typeahead.Typeahead = zk.$extends(zk.Widget, {
     bind_: function () {
         this.$supers(zul.typeahead.Typeahead, 'bind_', arguments);
         console.log('Typeahead.js: preparing substring matcher');
-        var substringMatcher = function (strs) {
-            return function findMatches(q, cb) {
-                var matches, substringRegex;
-
-                // an array that will be populated with substring matches
-                matches = [];
-
-                // regex used to determine if a string contains the substring `q`
-                substrRegex = new RegExp(q, 'i');
-
-                // iterate through the pool of strings and for any string that
-                // contains the substring `q`, add it to the `matches` array
-                $.each(strs, function (i, str) {
-                    if (substrRegex.test(str)) {
-                        matches.push(str);
-                    }
-                });
-
-                cb(matches);
-            };
-        };
         var states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
             'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii',
             'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana',
@@ -33,6 +12,11 @@ zul.typeahead.Typeahead = zk.$extends(zk.Widget, {
             'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
             'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
         ];
+        var statehound = new Bloodhound({
+            datumTokenizer: Bloodhound.tokenizers.whitespace,
+            queryTokenizer: Bloodhound.tokenizers.whitespace,
+            local: states
+        });
         console.log('Typeahead.js: initializing typeahead');
         $('#' + this.uuid).typeahead({
                 hint: true,
@@ -41,7 +25,7 @@ zul.typeahead.Typeahead = zk.$extends(zk.Widget, {
             },
             {
                 name: 'states',
-                source: substringMatcher(states)
+                source: statehound
             });
     },
     domClass_: function (no) {
